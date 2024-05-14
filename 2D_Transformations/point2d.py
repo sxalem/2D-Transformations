0ccdb5cb6663948cf4ea057a40e4f53d33d10dc0
sxalem <167420289+sxalem@users.noreply.github.com>
2024-05-14 09:44:38 +0200

    Update point2d.py

--- 2D_Transformations/point2d.py
+++ 2D_Transformations/point2d.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 
 class Point2D:
-    points = []  # Class variable to store all points
-    colors = []  # Class variable to store corresponding colors
+    points = []  
+    colors = []  
 
-    def __init__(self, x=0, y=0):  # Default values for x and y
+    def __init__(self, x=0, y=0):  
         self.x = x
         self.y = y
 
@@ -27,24 +27,24 @@ class Point2D:
             raise TypeError("Unsupported operand type for -: Point2D and {}".format(type(other)))
 
     def plot(self, color='ro'):
-        Point2D.points.append((self.x, self.y))  # Append point coordinates
-        Point2D.colors.append(color)             # Append color
+        Point2D.points.append((self.x, self.y)) 
+        Point2D.colors.append(color)             
 
     @classmethod
     def show_plot(cls):
-        plt.figure(figsize=(8, 6))  # Set figure size
+        plt.figure(figsize=(8, 6))  
         
         for i in range(len(cls.points)):
             x, y = cls.points[i]
             color = cls.colors[i]
-            plt.plot(x, y, color, marker='o', markersize=8),  # Plot point with larger marker size
-            plt.text(x, y, f'({x:.2f}, {y:.2f})', fontsize=12)  # Add text label with larger font size
+            plt.plot(x, y, color, marker='o', markersize=8),  
+            plt.text(x, y, f'({x:.2f}, {y:.2f})', fontsize=12)  
+    
+        plt.xlabel('X-axis', fontsize=14)  
+        plt.ylabel('Y-axis', fontsize=14)  
+        plt.title('2D Points Plot', fontsize=16)  
+        plt.grid(True)  
         
-        plt.xlabel('X-axis', fontsize=14)  # Set X-axis label with larger font size
-        plt.ylabel('Y-axis', fontsize=14)  # Set Y-axis label with larger font size
-        plt.title('2D Points Plot', fontsize=16)  # Set title with larger font size
-        plt.grid(True)  # Add grid
         
-        # Show plot
         plt.legend(['Original', 'Translated', 'Rotated', 'Transformed'], loc='lower right')
         plt.show()
