461816c47293e56465b90c020b8637201090e16c
sxalem <167420289+sxalem@users.noreply.github.com>
2024-05-14 09:39:45 +0200

    Update point2d.py

--- 2D_Transformations/point2d.py
+++ 2D_Transformations/point2d.py
@@ -46,5 +46,5 @@ class Point2D:
         plt.grid(True)  # Add grid
         
         # Show plot
-        plt.legend(['Original', 'Translated', 'Rotated', 'Transformed'], loc='upper left')
+        plt.legend(['Original', 'Translated', 'Rotated', 'Transformed'], loc='down right')
         plt.show()
