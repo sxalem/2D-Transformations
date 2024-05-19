4aad6ff3a04fe4d21a5b563da5775d639faafe73
sxalem <167420289+sxalem@users.noreply.github.com>
2024-05-19 18:05:12 +0200

    Add files via upload

---
+++ D_Transformations/transformation.py
@@ -0,0 +1,16 @@
+from D_Transformations.point2d import Point2D
+#trasnformation.py
+
+class Transformation:
+    def __init__(self, translate, rotate):
+        self.translate = translate
+        self.rotate = rotate
+    
+    def __mul__(self, point):
+        if isinstance(point, Point2D):
+            rotated_point = self.rotate * point
+            translated_point = self.translate * rotated_point
+            return translated_point
+        else:
+            raise TypeError("Unsupported operand type for *: Transformation and {}".format(type(point)))
+
